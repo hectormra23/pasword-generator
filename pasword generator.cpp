277d85283b6413b1bd004bdf8267b3b13a5e8de8
Hectormra23 <88894715+hectormra23@users.noreply.github.com>
2022-05-08 20:44:15 -0500

    Add files via upload

---
+++ pasword generator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+static const char alphanum[] =
+"0123456789"
+"!@#$%^&*"
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"abcdefghijklmnopqrstuvwxyz";
+int size = sizeof(alphanum) - 1;
+
+int main()
+{
+	//password length
+	int length = 8;
+	
+	srand(time(0));
+	cout<<"pasword:";
+	for (int i = 0; i < length; i++)
+	{
+		
+		cout << alphanum[rand() % ::size];
+	}
+	return 0;
+}
